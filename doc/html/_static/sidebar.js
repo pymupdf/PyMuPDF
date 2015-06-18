25ef4c1cb1b874fb14ac803ed4e08305b378b5f9
JorjMcKie <jorj.x.mckie@outlook.de>
2015-06-18 08:12:08 -0430

    Documentation update: Identity matrix is a constant
    Included cautioning comments on how (not) to use the Identity Matrix.

--- doc/html/_static/sidebar.js
+++ doc/html/_static/sidebar.js
@@ -16,20 +16,12 @@
  * Once the browser is closed the cookie is deleted and the position
  * reset to the default (expanded).
  *
- * :copyright: Copyright 2007-2014 by the Sphinx team, see AUTHORS.
+ * :copyright: Copyright 2007-2011 by the Sphinx team, see AUTHORS.
  * :license: BSD, see LICENSE for details.
  *
  */
 
 $(function() {
-  
-  
-  
-  
-  
-  
-  
-
   // global elements used by the functions.
   // the 'sidebarbutton' element is defined as global after its
   // creation, in the add_sidebar_button function
@@ -142,7 +134,7 @@ $(function() {
     var items = document.cookie.split(';');
     for(var k=0; k<items.length; k++) {
       var key_val = items[k].split('=');
-      var key = key_val[0].replace(/ /, "");  // strip leading spaces
+      var key = key_val[0];
       if (key == 'sidebar') {
         var value = key_val[1];
         if ((value == 'collapsed') && (!sidebar_is_collapsed()))
