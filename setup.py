c9849f75249e2a78503711efacedbfa9a9a87cfb
Ruikai Liu <lrk700@gmail.com>
2016-03-04 16:53:05 +0800

    update setup.py for OSX

--- setup.py
+++ setup.py
@@ -15,6 +15,20 @@ if sys.platform.startswith('linux'):
                                   'jbig2dec', 'openjp2', 'jpeg',
                                   'freetype'], # the libraries to link with
                       )
+elif sys.platform.startswith('darwin'):
+    module = Extension('fitz._fitz', # name of the module
+                       ['fitz/fitz_wrap.c'], # C source file
+                       # this is directory containing mupdf's header files
+                       include_dirs=['/usr/local/Cellar/mupdf-tools/1.8/include/mupdf/'],
+                       library_dirs=['/usr/local/Cellar/mupdf-tools/1.8/lib/',
+                                    '/usr/local/Cellar/openssl/1.0.2g/lib/',
+                                    '/usr/local/Cellar/jpeg/8d/lib/',
+                                    '/usr/local/Cellar/freetype/2.6.3/lib/',
+                                    '/usr/local/Cellar/jbig2dec/0.12/lib/'
+                           ],
+                       libraries=['mupdf', 'crypto', 'jpeg', 'freetype', 'jbig2dec']
+                      )
+
 else:
 #===============================================================================
 # This will build / set up PyMuPDF under Windows.
