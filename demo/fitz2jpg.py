e57fc06b56fbd212dc3a990cf0a355171bd17542
Jorj X. McKie <jorj.x.mckie@outlook.de>
2016-04-16 11:14:29 -0430

    Turned into a generalized Code Snippet

--- demo/fitz2jpg.py
+++ demo/fitz2jpg.py
@@ -5,17 +5,9 @@ from PIL import Image
 import sys
 from __future__ import print_function
 '''
-demonstrates how to output a JPEG image from PyMuPDF using PIL / Pillow
+Given any pixmap, use Pil / Pillow to save it in a different format
+Example: JPEG
 '''
-if len(sys.argv) == 2:
-    pic_fn = sys.argv[1]
-else:
-    pic_fn = None
-
-if pic_fn:
-    print(pic_fn)
-    pic = open(pic_fn, "rb").read()
-
-    pix = fitz.Pixmap(pic, len(pic))
-    img = Image.frombytes("RGBA",[pix.width, pix.height], str(pix.samples))
-    img.save(pic_fn + ".jpg", "jpeg")
+pix = fitz.Pixmap(...)
+img = Image.frombytes("RGBA",[pix.width, pix.height], str(pix.samples))
+img.save("filename.jpg", "jpeg")
