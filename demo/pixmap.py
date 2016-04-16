9c3a09d7a7e3fc092c89faf767fae7149ddb2699
Jorj X. McKie <jorj.x.mckie@outlook.de>
2016-04-16 11:03:20 -0430

    Use V1.9 options to create Pixmaps

--- demo/pixmap.py
+++ demo/pixmap.py
@@ -15,16 +15,15 @@ Demonstrates some of MuPDF's non-PDF graphic capabilities.
 Read a PNG image and create a new one consisting of 3 * 4 tiles of it.
 ===============================================================================
 '''
-# read in picture image as a pixmap
-pic = open("editra.png", "rb").read()
-pix0 = fitz.Pixmap(pic, len(pic))
-
-# calculate target pixmap dimensions and then create it
+# create pixmap from file
+pix0 = fitz.getPixmapFromFile("editra.png")
+# calculate target pixmap colorspace and dimensions and then create it
+tar_cs     = pix0.getColorspace()
 tar_width  = pix0.width * 3
 tar_height = pix0.height * 4
 tar_irect  = fitz.IRect(0, 0, tar_width, tar_height)
-tar_pix    = fitz.Pixmap(fitz.Colorspace(fitz.CS_RGB), tar_irect)
-tar_pix.clearWith(90)        # clear target with a lively gray :-)
+tar_pix    = fitz.Pixmap(tar_cs, tar_irect)
+tar_pix.clearWith(90)        # clear target with a "very lively stone gray" (Loriot)
 
 # now fill target with 3 * 4 tiles of input picture
 for i in list(range(4)):
@@ -32,6 +31,6 @@ for i in list(range(4)):
     for j in list(range(3)):
         pix0.x = j * pix0.width                       # modify input's x coord
         tar_pix.copyPixmap(pix0, pix0.getIRect())     # copy input to new loc
-        # save intermediate image to show what is happening
+        # save also intermediate images to show what is happening
         fn = "target-" + str(i) + str(j) + ".png"
         tar_pix.writePNG(fn)
