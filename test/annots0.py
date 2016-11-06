6b7c76a3445831b3b8c84481286c511aaab9705b
Jorj X. McKie <jorj.x.mckie@outlook.de>
2016-11-06 08:04:41 -0400

    Adding test data

--- test/annots0.py
+++ test/annots0.py
@@ -1,1 +1,21 @@
+#! python
+from __future__ import print_function
+import fitz
 
+datei = "test.pdf"
+doc = fitz.open(datei)
+p0 = doc[0]
+a = p0.firstAnnot
+da = dir(a)
+i=0
+while a:
+    print("=".ljust(80,"="))
+    for x in da:
+        if x.startswith("_") or x in ["next", "super", "this", "page", "getPixmap"]:
+            continue
+        s = "a." + x
+        print (x.ljust(12), "====>", eval(s))
+    pix = a.getPixmap()
+    pix.writePNG("a-%s.png" % (i,))
+    i += 1
+    a = a.next
