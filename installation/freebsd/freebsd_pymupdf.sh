7f7eff606d3f5ed5e2fe45592aec9ff73e010d4d
wenheping <wenheping2000@hotmail.com>
2020-09-11 10:46:53 +0800

    Update freebsd_pymupdf.sh

--- installation/freebsd/freebsd_pymupdf.sh
+++ installation/freebsd/freebsd_pymupdf.sh
@@ -1,18 +1,20 @@
 setenv CFLAGS -fPIC
 
 # install the pre-required tool
-pkg install swig30
+pkg install swig
 
 # also FreeBSD has a mupdf port, we should build it manually
-wget https://mupdf.com/downloads/archive/mupdf-1.16.1-source.tar.gz
-tar -zxvf mupdf-1.16.1-source.tar.gz
+wget https://mupdf.com/downloads/archive/mupdf-1.17.0-source.tar.gz
+tar -zxvf mupdf-1.17.0-source.tar.gz
 
 rm -rf PyMuPDF
 git clone https://github.com/pymupdf/PyMuPDF.git
 
-cd mupdf-1.16.1-source
+cd mupdf-1.17.0-source
 # replace files in mupdf source
 cp ../PyMuPDF/fitz/_config.h include/mupdf/fitz/config.h
+cp ../PyMuPDF/fitz/_pdf-font-add.c source/pdf/pdf-font-add.c
+cp ../PyMuPDF/fitz/_encode-fax.c source/fitz/encode-fax.c
 
 gmake HAVE_X11=no HAVE_GLFW=no HAVE_GLUT=no prefix=/usr/local
 gmake HAVE_X11=no HAVE_GLFW=no HAVE_GLUT=no prefix=/usr/local install
