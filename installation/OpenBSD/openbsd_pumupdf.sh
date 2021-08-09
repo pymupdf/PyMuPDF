ea971f9f15743bae21a3f208098bd96a0c2f0fe6
wenheping <wen@FreeBSD.org>
2021-08-09 17:08:47 +0800

    Create openbsd_pumupdf.sh

---
+++ installation/OpenBSD/openbsd_pumupdf.sh
@@ -0,0 +1,23 @@
+export CFLAGS=-fPIC
+
+# install the pre-required tool
+pkg install swig git gmake python3 wget
+
+wget https://mupdf.com/downloads/archive/mupdf-1.18.0-source.tar.gz
+tar -zxvf mupdf-1.18.0-source.tar.gz
+
+rm -rf PyMuPDF
+git clone https://github.com/pymupdf/PyMuPDF.git
+
+cd mupdf-1.18.0-source
+# replace files in mupdf source
+cp ../PyMuPDF/fitz/_config.h include/mupdf/fitz/config.h
+
+ln -s /usr/bin/c++ /usr/bin/g++
+
+gmake HAVE_X11=no HAVE_GLFW=no HAVE_GLUT=no prefix=/usr/local
+gmake HAVE_X11=no HAVE_GLFW=no HAVE_GLUT=no prefix=/usr/local install
+
+cd ../PyMuPDF
+python3 setup.py build
+python3 setup.py install
