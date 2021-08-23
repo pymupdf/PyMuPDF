980674835059c10af9076686569e0cccac48c6e9
Jorj McKie <jorj.x.mckie@outlook.de>
2021-08-23 05:18:14 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -163,7 +163,7 @@ long_desc = "\n".join(long_dtab)
 
 setup(
     name="PyMuPDF",
-    version="1.18.16",
+    version="1.18.17",
     description="Python bindings for the PDF toolkit and renderer MuPDF",
     long_description=long_desc,
     classifiers=classifier,
