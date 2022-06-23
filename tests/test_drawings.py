812b5d995873373270a1e509fbb2e3848f96a282
Julian Smith <julian.smith@artifex.com>
2022-06-23 14:20:50 +0100

    Fix #1751: closePath is always False.
    fitz/helper-devices.i:
        trace_curveto(): also set closePath to true in the case where dev_linecount
        is 3 but jm_checkrect() does not modify the path.
    
        jm_tracedraw_stroke_path(): set default closePath=False before running
        jm_tracedraw_path() (which sometimes sets it to True). Previously we would
        always reset it to False.
    
    tests/test_drawings.py:
        Added test that creating closed paths results in get_drawings() returning
        closePath=True.
    
    tests/resources/symbols.txt:
        Updated to contain "'closePath': True'" items to match new behaviour.
    
    Thanks to @Luux for bug report with reproducer.

--- tests/test_drawings.py
+++ tests/test_drawings.py
@@ -3,7 +3,8 @@ Extract drawings of a PDF page and compare with stored expected result.
 """
 import io
 import os
-from pprint import pprint
+import sys
+import pprint
 
 import fitz
 
@@ -18,7 +19,7 @@ def test_drawings1():
     page = doc[0]
     paths = page.get_cdrawings()
     out = io.StringIO()  # pprint output goes here
-    pprint(paths, stream=out)
+    pprint.pprint(paths, stream=out)
     assert symbols_text == out.getvalue()
 
 
@@ -59,3 +60,83 @@ def test_drawings2():
     bboxes = [b[1] for b in page.get_bboxlog()]
     for i, r in enumerate(rects):
         assert fitz.Rect(r) in fitz.Rect(bboxes[i])
+
+
+def _dict_difference(a, b):
+    '''
+    Returns `(keys_a, keys_b, key_values)`, information about differences
+    between dicts `a` and `b`.
+    
+    `keys_a` is the set of keys that are in `a` but not in `b`.
+
+    `keys_b` is the set of keys that are in `b` but not in `a`.
+
+    `key_values` is a dict with keys that are in both `a` and `b` but where the
+    values differ; the values in this dict are `(value_a, value_b)`.
+    '''
+    keys_a = set()
+    keys_b = set()
+    key_values = dict()
+    for key in a:
+        if key not in b:
+            keys_a.add( key)
+    for key in b:
+        if key not in a:
+            keys_b.add( key)
+    for key, va in a.items():
+        if key in b:
+            vb = b[key]
+            if va != vb:
+                key_values[key] = (va, vb)
+    return keys_a, keys_b, key_values
+
+
+def test_drawings3():
+    doc = fitz.open()
+
+    page1 = doc.new_page()
+    shape1 = page1.new_shape()
+    shape1.draw_line((10, 10), (10, 50))
+    shape1.draw_line((10, 50), (100, 100))
+    shape1.finish(closePath=False, color=(0,0,0), width=5)
+    shape1.commit()
+    drawings1 = list(page1.get_drawings())
+
+    page2 = doc.new_page()
+    shape2 = page2.new_shape()
+    shape2.draw_line((10, 10), (10, 50))
+    shape2.draw_line((10, 50), (100, 100))
+    shape2.finish(closePath=True, color=(0,0,0), width=5)
+    shape2.commit()
+    drawings2 = list(page2.get_drawings())
+
+    page3 = doc.new_page()
+    shape3 = page3.new_shape()
+    shape3.draw_line((10, 10), (10, 50))
+    shape3.draw_line((10, 50), (100, 100))
+    shape3.draw_line((100, 100), (50, 70))
+    shape3.finish(closePath=False, color=(0,0,0), width=5)
+    shape3.commit()
+    drawings3 = list(page3.get_drawings())
+
+    page4 = doc.new_page()
+    shape4 = page4.new_shape()
+    shape4.draw_line((10, 10), (10, 50))
+    shape4.draw_line((10, 50), (100, 100))
+    shape4.draw_line((100, 100), (50, 70))
+    shape4.finish(closePath=True, color=(0,0,0), width=5)
+    shape4.commit()
+    drawings4 = list(page4.get_drawings())
+
+    assert len(drawings1) == len(drawings2) == 1
+    drawings1 = drawings1[0]
+    drawings2 = drawings2[0]
+    diff = _dict_difference( drawings1, drawings2)
+    assert diff == (set(), set(), {'closePath': (False, True)})
+    
+    assert len(drawings3) == len(drawings4) == 1
+    drawings3 = drawings3[0]
+    drawings4 = drawings4[0]
+    diff = _dict_difference( drawings3, drawings4)
+    assert diff == (set(), set(), {'closePath': (False, True)})
+    
