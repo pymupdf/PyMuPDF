f1cdfdc240b7e1bf77e0d906dbb29f81041d2883
Julian Smith <julian.smith@artifex.com>
2022-06-24 18:56:00 +0100

    docs/conf.py: disable html footer showing sphinx and theme.

--- docs/conf.py
+++ docs/conf.py
@@ -162,7 +162,7 @@ html_split_index = True
 html_show_sourcelink = True
 html_sourcelink_suffix = ".rst"
 # If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
-html_show_sphinx = True
+html_show_sphinx = False
 
 # If true, "(C) Copyright ..." is shown in the HTML footer. Default is True.
 html_show_copyright = True
