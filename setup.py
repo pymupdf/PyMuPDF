6efca61a05002028285d0b093c9f2ca3674aa4e1
Julian Smith <julian.smith@artifex.com>
2022-08-12 23:36:22 +0100

    setup.py: build with mupdf-1.20.3.

--- setup.py
+++ setup.py
@@ -389,7 +389,7 @@ def get_mupdf_tgz():
     '''
     mupdf_url_or_local = os.environ.get(
             'PYMUPDF_SETUP_MUPDF_TGZ',
-            'https://mupdf.com/downloads/archive/mupdf-1.20.0-source.tar.gz',
+            'https://mupdf.com/downloads/archive/mupdf-1.20.3-source.tar.gz',
             )
     log( f'mupdf_url_or_local={mupdf_url_or_local!r}')
     if mupdf_url_or_local == '':
