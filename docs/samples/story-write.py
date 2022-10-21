d4fd422319d8da9019e9871c2cf64bf3b57799e4
Julian Smith <julian.smith@artifex.com>
2022-10-21 15:58:21 +0100

    docs/samples/story-*.py: demonstrations of alternative Story API.

---
+++ docs/samples/story-write.py
@@ -0,0 +1,81 @@
+"""
+Demo script for PyMuPDF's `Story.write()` method.
+
+This is a way of laying out a story into a PDF document, that avoids the need
+to write a loop that calls `story.place()` and `story.draw()`.
+
+Instead just a single function call is required, albeit with a `rectfn()`
+callback that returns the rectangles into which the story is placed.
+"""
+
+import html
+
+import fitz
+
+
+with open(__file__) as f:
+    text = f.read()
+
+text = html.escape(text)
+
+html = f'''
+<!DOCTYPE html>
+<body>
+
+<h1>Contents of {__file__}</h1>
+
+<h2>Normal</h2>
+<pre>
+{text}
+</pre>
+
+<h2>Strong</h2>
+<strong>
+<pre>
+{text}
+</pre>
+</strong>
+
+<h2>Em</h2>
+<em>
+<pre>
+{text}
+</pre>
+</em>
+
+</body>
+'''
+
+
+def rectfn(rect_num, filled):
+    '''
+    We return four rectangles per page in this order:
+    
+        1 3
+        2 4
+    '''
+    page_w = 800
+    page_h = 600
+    margin = 50
+    rect_w = (page_w - 3*margin) / 2
+    rect_h = (page_h - 3*margin) / 2
+    
+    if rect_num % 4 == 0:
+        # New page.
+        mediabox = fitz.Rect(0, 0, page_w, page_h)
+    else:
+        mediabox = None
+    # Return one of four rects in turn.
+    rect_x = margin + (rect_w+margin) * ((rect_num // 2) % 2)
+    rect_y = margin + (rect_h+margin) * (rect_num % 2)
+    rect = fitz.Rect(rect_x, rect_y, rect_x + rect_w, rect_y + rect_h)
+    #print(f'rectfn(): rect_num={rect_num} filled={filled}. Returning: rect={rect}')
+    return mediabox, rect, None
+
+story = fitz.Story(html, em=8)
+
+out_path = __file__.replace('.py', '.pdf')
+writer = fitz.DocumentWriter(out_path)
+
+story.write(writer, rectfn)
+writer.close()
