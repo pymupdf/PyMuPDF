7b5cfc6ab89ea0e4ee94c04acbbf4e6d7313d6ef
Julian Smith <julian.smith@artifex.com>
2023-04-12 21:51:10 +0100

    fitz/__init__.py: allow use with previous version of mupdf.
    While we are waiting for new mupdf release, vanilla build+test uses mupdf-1.21,
    next release will use mupdf-1.22, and mupdf-master is 1.23, so we need to allow
    use with all three mupdf versions.

--- fitz/__init__.py
+++ fitz/__init__.py
@@ -66,13 +66,22 @@ def v_tuple_to_string(t):
 
 mupdf_version_tuple = v_str_to_tuple(fitz.TOOLS.mupdf_version())
 mupdf_version_tuple_required = v_str_to_tuple(fitz.VersionFitz)
+mupdf_version_tuple_required_prev = (mupdf_version_tuple_required[0], mupdf_version_tuple_required[1]-1)
 mupdf_version_tuple_required_next = (mupdf_version_tuple_required[0], mupdf_version_tuple_required[1]+1)
 
 if mupdf_version_tuple[:2] not in (
+        mupdf_version_tuple_required_prev[:2],
         mupdf_version_tuple_required[:2], 
         mupdf_version_tuple_required_next[:2],
         ):
-    raise ValueError(f'MuPDF library {v_tuple_to_string(mupdf_version_tuple)!r} mismatch: require {v_tuple_to_string(mupdf_version_tuple_required)!r} or (master) {v_tuple_to_string(mupdf_version_tuple_required_next)!r}.')
+    raise ValueError(
+            f'MuPDF library {v_tuple_to_string(mupdf_version_tuple)!r} mismatch:'
+            f' require'
+            f' {v_tuple_to_string(mupdf_version_tuple_required_prev)!r}'
+            f' or {v_tuple_to_string(mupdf_version_tuple_required)!r}'
+            f' or {v_tuple_to_string(mupdf_version_tuple_required_next)!r}'
+            f'.'
+            )
 
 # copy functions in 'utils' to their respective fitz classes
 import fitz.utils
