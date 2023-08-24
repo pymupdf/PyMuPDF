d5a8b79d9fc96ed6fc42a16314abcc71711a65b9
Julian Smith <julian.smith@artifex.com>
2023-08-24 11:28:59 +0100

    More updates to version and dates for release of 1.23.1.

--- fitz/version.i
+++ fitz/version.i
@@ -1,6 +1,6 @@
 %pythoncode %{
 VersionFitz = "1.23.0" # MuPDF version.
 VersionBind = "1.23.1" # PyMuPDF version.
-VersionDate = "2023-08-23 00:00:01"
-version = (VersionBind, VersionFitz, "20230823000001")
+VersionDate = "2023-08-24 00:00:01"
+version = (VersionBind, VersionFitz, "20230824000001")
 %}
--- src/__init__.py
+++ src/__init__.py
@@ -21235,7 +21235,7 @@ class FitzDeprecation(DeprecationWarning):
 
 VersionFitz = "1.23.0" # MuPDF version.
 VersionBind = "1.23.1" # PyMuPDF version.
-VersionDate = "2023-08-23 00:00:01"
+VersionDate = "2023-08-24 00:00:01"
 VersionDate2 = VersionDate.replace('-', '').replace(' ', '').replace(':', '')
 version = (VersionBind, VersionFitz, VersionDate2)
 
