e9d58d7ff31bea1943bd2610d8d9168c92ca8c7e
Julian Smith <julian.smith@artifex.com>
2023-10-05 19:01:40 +0100

    Added test for #2548 and address rebase fail.
    Fixed test failure in rebased. We were not converting fz exception into C++
    exception in src/extra.i:page_get_textpage(). Also fixed other cases where we
    leaked fz exception.

---
+++ tests/test_2548.py
@@ -0,0 +1,27 @@
+import os
+
+import fitz
+
+root = os.path.abspath(f'{__file__}/../..')
+
+def test_2548():
+    """Text extraction should fail because of PDF structure cycle.
+
+    Old MuPDF version did not detect the loop.
+    """
+    doc = fitz.open(f'{root}/tests/resources/test_2548.pdf')
+    e = False
+    for page in doc:
+        try:
+            _ = page.get_text()
+        except Exception as ee:
+            print(f'test_2548: {ee=}')
+            if hasattr(fitz, 'mupdf'):
+                # Rebased.
+                expected = "RuntimeError('code=2: cycle in structure tree')"
+            else:
+                # Classic.
+                expected = "RuntimeError('cycle in structure tree')"
+            assert repr(ee) == expected, f'Expected {expected=} but got {repr(ee)=}.'
+            e = True
+    assert e
