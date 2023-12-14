eb00a2847429c0ed23b862398a06834f6f1f8c59
Julian Smith <julian.smith@artifex.com>
2023-12-14 15:58:01 +0000

    src/__init__.py: class JM_new_output_fileptr_Output: use explicit args instead of (*args, **kwargs).

--- src/__init__.py
+++ src/__init__.py
@@ -19091,15 +19091,15 @@ class JM_new_output_fileptr_Output(mupdf.FzOutput2):
         self.use_virtual_tell()
         self.use_virtual_truncate()
     
-    def seek( self, ctx, *args, **kwargs):
-        return self.bio.seek( *args, **kwargs)
+    def seek( self, ctx, offset, whence):
+        return self.bio.seek( offset, whence)
     
-    def tell( self, ctx, *args, **kwargs):
-        ret = self.bio.tell( *args, **kwargs)
+    def tell( self, ctx):
+        ret = self.bio.tell()
         return ret
     
-    def truncate( self, ctx, *args, **kwargs):
-        return self.bio.truncate( *args, **kwargs)
+    def truncate( self, ctx):
+        return self.bio.truncate()
     
     def write(self, ctx, data_raw, data_length):
         data = mupdf.raw_to_python_bytes(data_raw, data_length)
