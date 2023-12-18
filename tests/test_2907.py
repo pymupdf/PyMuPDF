9cf377a9a78410962585fc220901a8b6eb9c668a
Julian Smith <julian.smith@artifex.com>
2023-12-18 18:26:13 +0000

    tests/: added test_2907() for segfault bug in classic.
    This is for #2907, 'segfault trying to call clean_contents on certain pdfs with
    python 3.12'.
    
    We are not intending to fix this bug, so actually this test only runs on
    rebased.

---
+++ tests/test_2907.py
@@ -0,0 +1,18 @@
+import fitz
+
+import os.path
+import pathlib
+
+def test_2907():
+    # This test is for a bug in classic 'segfault trying to call clean_contents
+    # on certain pdfs with python 3.12', which we are not going to fix.
+    if not hasattr(fitz, 'mupdf'):
+        print('test_2907(): not running on classic because known to fail.')
+        return
+    path = os.path.abspath(f'{__file__}/../../tests/resources/test_2907.pdf')
+    pdf_file = pathlib.Path(path).read_bytes()
+    fitz_document = fitz.open(stream=pdf_file, filetype="application/pdf")
+
+    pdf_pages = list(fitz_document.pages())
+    (page,) = pdf_pages
+    page.clean_contents()
