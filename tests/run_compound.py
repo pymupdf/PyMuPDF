8c8eaca9724ab441140d78413eb865eb0a338c23
Julian Smith <julian.smith@artifex.com>
2023-12-20 00:06:54 +0000

    rebase stage 2: tests/run_compound.py: updated to use fitz_old and fitz.

--- tests/run_compound.py
+++ tests/run_compound.py
@@ -3,9 +3,9 @@
 '''
 Runs a command three times:
 
-1.  Run unchanged. This will use the default `fitz` implementation of PyMuPDF.
+1.  Run unchanged. This will use the default `fitz_old` implementation of PyMuPDF.
 
-2.  Run with PYTHONPATH set up to use the `fitz_new` implementation of PyMuPDF.
+2.  Run with PYTHONPATH set up to use the `fitz` implementation of PyMuPDF.
 
 3.  As 2 but also set PYMUPDF_USE_EXTRA=0 to disable use of C++ optimisations.
 
@@ -33,14 +33,9 @@ def main():
 
     args = sys.argv[1:]
     
-    # Run with default `fitz`.
-    #
-    log( f'Running using fitz: {shlex.join(args)}')
-    e1 = subprocess.run( args, shell=0, check=0).returncode
-    
-    # Run with `fitz_new`. We create a file fitz.py that does `from fitz_new
+    # Run with `fitz_old` (classic). We create a file fitz.py that does `from fitz_old
     # import *` and prepend it to PYTHONPATH. So `import fitz` will actually
-    # import fitz_new as fitz.
+    # import fitz_old as fitz.
     #
     d = os.path.abspath( f'{__file__}/../resources')
     
@@ -51,22 +46,28 @@ def main():
         f.write( textwrap.dedent( f'''
                 #import sys
                 #print(f'{{__file__}}: {{sys.path=}}')
-                #print(f'{{__file__}}: Importing * from fitz_new')
+                #print(f'{{__file__}}: Importing * from fitz_old')
                 #sys.stdout.flush()
-                from fitz_new import *
+                from fitz_old import *
                 '''))
     
     env = os.environ.copy()
     pp = env.get( 'PYTHONPATH')
     pp = d if pp is None else f'{d}:{pp}'
     env[ 'PYTHONPATH'] = pp
-    log(f'Running using fitz_new, PYTHONPATH={pp}: {shlex.join(args)}')
+    log(f'Running using fitz_old (classic), PYTHONPATH={pp}: {shlex.join(args)}')
     e2 = subprocess.run( args, shell=0, check=0, env=env).returncode
     
-    # Run with `fitz_new` again, this time with PYMUPDF_USE_EXTRA=0.
+    # Run with default `fitz` (rebased).
     #
+    log( f'Running using fitz (rebased): {shlex.join(args)}')
+    e1 = subprocess.run( args, shell=0, check=0).returncode
+    
+    # Run with `fitz` (rebased) again, this time with PYMUPDF_USE_EXTRA=0.
+    #
+    env = os.environ.copy()
     env[ 'PYMUPDF_USE_EXTRA'] = '0'
-    log(f'Running using fitz_new without optimisations, PYTHONPATH={pp}: {shlex.join(args)}')
+    log(f'Running using fitz (rebased) without optimisations, PYTHONPATH={pp}: {shlex.join(args)}')
     e3 = subprocess.run( args, shell=0, check=0, env=env).returncode
     
     log( f'{e1=} {e2=} {e3=}')
