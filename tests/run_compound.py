6c9b340d0ccb80ae71ee19fa037f4d2a27852acc
Julian Smith <julian.smith@artifex.com>
2023-12-21 17:14:31 +0000

    tests/run_compound.py: added option to select which implementations to run.
    Also don't print python backtrace after subprocess errors, as not usually
    useful.

--- tests/run_compound.py
+++ tests/run_compound.py
@@ -3,15 +3,24 @@
 '''
 Runs a command three times:
 
-1.  Run unchanged. This will use the default `fitz_old` implementation of PyMuPDF.
+1.  Run using classic implementation of PyMuPDF, by setting PYTHONPATH. This
+    will use the `fitz_old` implementation of PyMuPDF.
 
-2.  Run with PYTHONPATH set up to use the `fitz` implementation of PyMuPDF.
+2.  Run with rebased implementation of PyMuPDF.
 
 3.  As 2 but also set PYMUPDF_USE_EXTRA=0 to disable use of C++ optimisations.
 
 Example usage:
 
     ./PyMuPDF/tests/run_compound.py python -m pytest -s PyMuPDF
+
+Use `-i <implementations>` to select which implementations to use. In
+`<implementations>`, `r` means rebased, `R` means rebased without
+optimisations, `c` means classic.
+
+For example use only the rebased and unoptimised rebased implementations with:
+
+    ./PyMuPDF/tests/run_compound.py -i rR python -m pytest -s PyMuPDF
 '''
 
 import shlex
@@ -22,59 +31,117 @@ import sys
 import textwrap
 
 
+
+
 def log(text):
-    print('#' * 40)
-    print(f'{__file__} python-{platform.python_version()}: {text}')
-    print('#' * 40)
+    print(textwrap.indent(text, 'PyMuPDF:tests/run_compound.py: '))
     sys.stdout.flush()
+
+
+def log_star(text):
+    log('#' * 40)
+    log(text)
+    log('#' * 40)
 
 
 def main():
 
-    args = sys.argv[1:]
+    implementations = 'crR'
+    i = 1
+    while i < len(sys.argv):
+        arg = sys.argv[i]
+        if arg == '-i':
+            i += 1
+            implementations = sys.argv[i]
+        elif arg.startswith('-'):
+            raise Exception(f'Unrecognised {arg=}.')
+        else:
+            break
+        i += 1
+    args = sys.argv[i:]
     
-    # Run with `fitz_old` (classic). We create a file fitz.py that does `from fitz_old
-    # import *` and prepend it to PYTHONPATH. So `import fitz` will actually
-    # import fitz_old as fitz.
-    #
-    d = os.path.abspath( f'{__file__}/../resources')
+    e_classic = None
+    e_rebased = None
+    e_rebased_unoptimised = None
     
-    # [Must not do `d = os.path.relpath(d)` because it fails on Windows if
-    # __file__ is on different drive from cwd.]
+    # Check `implementations`.
+    implementations_seen = set()
+    for i in implementations:
+        assert i not in implementations_seen, f'Duplicate implementation {i!r} in {implementations!r}.'
+        if i == 'c':
+            name = 'classic'
+        elif i == 'r':
+            name = 'rebased'
+        elif i == 'R':
+            name = 'rebased (unoptimised)'
+        else:
+            assert 0, f'Unrecognised implementation {i!r} in {implementations!r}.'
+        log(f'    {i!r}: will run with PyMuPDF {name}.')
+        implementations_seen.add(i)
     
-    with open( f'{d}/fitz.py', 'w') as f:
-        f.write( textwrap.dedent( f'''
-                #import sys
-                #print(f'{{__file__}}: {{sys.path=}}')
-                #print(f'{{__file__}}: Importing * from fitz_old')
-                #sys.stdout.flush()
-                from fitz_old import *
-                '''))
+    for i in implementations:
+        if i == 'c':
+            # Run with `fitz_old` (classic). We create a file fitz.py that does `from fitz_old
+            # import *` and prepend it to PYTHONPATH. So `import fitz` will actually
+            # import fitz_old as fitz.
+            #
+            d = os.path.abspath( f'{__file__}/../resources')
     
-    env = os.environ.copy()
-    pp = env.get( 'PYTHONPATH')
-    pp = d if pp is None else f'{d}:{pp}'
-    env[ 'PYTHONPATH'] = pp
-    log(f'Running using fitz_old (classic), PYTHONPATH={pp}: {shlex.join(args)}')
-    e2 = subprocess.run( args, shell=0, check=0, env=env).returncode
+            # [Must not do `d = os.path.relpath(d)` because it fails on Windows if
+            # __file__ is on different drive from cwd.]
     
-    # Run with default `fitz` (rebased).
-    #
-    log( f'Running using fitz (rebased): {shlex.join(args)}')
-    e1 = subprocess.run( args, shell=0, check=0).returncode
+            with open( f'{d}/fitz.py', 'w') as f:
+                f.write( textwrap.dedent( f'''
+                        #import sys
+                        #print(f'{{__file__}}: {{sys.path=}}')
+                        #print(f'{{__file__}}: Importing * from fitz_old')
+                        #sys.stdout.flush()
+                        from fitz_old import *
+                        '''))
     
-    # Run with `fitz` (rebased) again, this time with PYMUPDF_USE_EXTRA=0.
-    #
-    env = os.environ.copy()
-    env[ 'PYMUPDF_USE_EXTRA'] = '0'
-    log(f'Running using fitz (rebased) without optimisations, PYTHONPATH={pp}: {shlex.join(args)}')
-    e3 = subprocess.run( args, shell=0, check=0, env=env).returncode
+            env = os.environ.copy()
+            pp = env.get( 'PYTHONPATH')
+            pp = d if pp is None else f'{d}:{pp}'
+            env[ 'PYTHONPATH'] = pp
+            log_star(f'Running using fitz_old (classic), PYTHONPATH={pp}: {shlex.join(args)}')
+            e_classic = subprocess.run( args, shell=0, check=0, env=env).returncode
+        
+        elif i == 'r':
     
-    log( f'{e1=} {e2=} {e3=}')
+            # Run with default `fitz` (rebased).
+            #
+            log_star( f'Running using fitz (rebased): {shlex.join(args)}')
+            e_rebased = subprocess.run( args, shell=0, check=0).returncode
     
-    if e1 or e2 or e3:
-        raise Exception( f'Failure: {e1=} {e2=} {e3=}')
+        elif i == 'R':
+    
+            # Run with `fitz` (rebased) again, this time with PYMUPDF_USE_EXTRA=0.
+            #
+            env = os.environ.copy()
+            env[ 'PYMUPDF_USE_EXTRA'] = '0'
+            log_star(f'Running using fitz (rebased) with PYMUPDF_USE_EXTRA=0: {shlex.join(args)}')
+            e_rebased_unoptimised = subprocess.run( args, shell=0, check=0, env=env).returncode
+        
+        else:
+            raise Exception(f'Unrecognised implementation {i!r}.')
+    
+    if e_classic is not None:
+        log(f'{e_classic=}')
+    if e_rebased is not None:
+        log(f'{e_rebased=}')
+    if e_rebased_unoptimised is not None:
+        log(f'{e_rebased_unoptimised=}')
+    
+    if e_classic or e_rebased or e_rebased_unoptimised:
+       log('Test(s) failed.')
+       return 1
 
 
 if __name__ == '__main__':
-    main()
+    try:
+        sys.exit(main())
+    except subprocess.CalledProcessError as e:
+        # Terminate relatively quietly, failed commands will usually have
+        # generated diagnostics.
+        log(e)
+        sys.exit(1)
