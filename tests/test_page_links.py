0e245bbe66dcf324071d7fbad147a12b5b2a9531
Niall French <niall@circuitmind.io>
2024-01-22 10:57:27 +0000

    Fix Page.links generator

---
+++ tests/test_page_links.py
@@ -0,0 +1,17 @@
+import fitz
+
+import os
+
+
+def test_page_links_generator():
+    # open some arbitrary PDF
+    path = os.path.abspath(f"{__file__}/../../tests/resources/2.pdf")
+    doc = fitz.open(path)
+
+    # select an arbitrary page
+    page = doc[-1]
+
+    # iterate over pages.links
+    link_generator = page.links()
+    links = list(link_generator)
+    assert len(links) == 7
