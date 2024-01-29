0a63b9c00459aa59b597d98e52a5c15288f88dc8
Julian Smith <julian.smith@artifex.com>
2024-01-29 15:29:52 +0000

    Updated version, changelog, and dates for release 1.23.20.

--- src/__init__.py
+++ src/__init__.py
@@ -21815,8 +21815,8 @@ def int_rc(text):
     return int(text)
 
 VersionFitz = "1.23.9" # MuPDF version.
-VersionBind = "1.23.19" # PyMuPDF version.
-VersionDate = "2024-01-25 00:00:01"
+VersionBind = "1.23.20" # PyMuPDF version.
+VersionDate = "2024-01-29 00:00:01"
 VersionDate2 = VersionDate.replace('-', '').replace(' ', '').replace(':', '')
 version = (VersionBind, VersionFitz, VersionDate2)
 pymupdf_version_tuple = tuple( [int_rc(i) for i in VersionBind.split('.')])
