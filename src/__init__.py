c34ac48f0c70d2b447f280f457b43e6372b41a95
psambit9791 <sambitpaul1992@gmail.com>
2024-02-16 01:25:15 +0000

    Modified util_make_irect() to capture area more robustly; updates issue #3163

--- src/__init__.py
+++ src/__init__.py
@@ -20490,13 +20490,15 @@ def util_make_rect( *args, p0=None, p1=None, x0=None, y0=None, x1=None, y1=None)
 
 def util_make_irect( *args, p0=None, p1=None, x0=None, y0=None, x1=None, y1=None):
     a, b, c, d = util_make_rect( *args, p0=p0, p1=p1, x0=x0, y0=y0, x1=x1, y1=y1)
-    def convert(x):
-        ret = int(x)
-        return ret
-    a = convert(a)
-    b = convert(b)
-    c = convert(c)
-    d = convert(d)
+    def convert(x, ceil):
+        if ceil:
+            return int(math.ceil(x))
+        else:
+            return int(math.floor(x))
+    a = convert(a, False)
+    b = convert(b, False)
+    c = convert(c, True)
+    d = convert(d, True)
     return a, b, c, d
 
 
