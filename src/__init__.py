4ffddf02cc1154a96b6718d486edc00dcc68ee35
Colin B. Macdonald <cbm@m.fsf.org>
2024-05-17 16:38:21 -0700

    Few more typos and exceptions

--- src/__init__.py
+++ src/__init__.py
@@ -17786,9 +17786,9 @@ def CheckMorph(o: typing.Any) -> bool:
     if not (type(o) in (list, tuple) and len(o) == 2):
         raise ValueError("morph must be a sequence of length 2")
     if not (len(o[0]) == 2 and len(o[1]) == 6):
-        raise ValueError("invalid morph parm 0")
+        raise ValueError("invalid morph param 0")
     if not o[1][4] == o[1][5] == 0:
-        raise ValueError("invalid morph parm 1")
+        raise ValueError("invalid morph param 1")
     return True
 
 
