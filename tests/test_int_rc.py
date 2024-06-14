32b4a9c23be4f3435234e569a1a8208ec1d66fd0
nehir <n.altinkaya@vu.nl>
2024-06-14 14:26:52 +0200

    last version of two tests

--- tests/test_int_rc.py
+++ tests/test_int_rc.py
@@ -19,11 +19,28 @@ def print_coverage():
     for branch, hit in branch_coverage.items():
         print(f"{branch} was {'hit' if hit else 'not hit'}")
 
-result = _int_rc("9876")
-print_coverage()
+def test_int_rc_text_positiveOrZero_returns_9876():
+    result = _int_rc("9876")
+    print_coverage()
 
-result = _int_rc("-333")
-print_coverage()
+def test_int_rc_text_positiveOrZero_returns_neg333():
+    result = _int_rc("-333")
+    print_coverage()
 
-result = _int_rc("333rc")
-print_coverage()
+def test_int_rc_text_is_not_positiveOrZero_returns_333():
+    result = _int_rc("333rc")
+    print_coverage()
+
+def test_int_rc_text_is_not_positiveOrZero_returns_321():
+    result = _int_rc("321rc555")
+    print_coverage()
+    
+def test_int_rc_text_is_not_positiveOrZero_returns_3456():
+    result = _int_rc("3456rc555rc")
+    print_coverage()
+
+test_int_rc_text_positiveOrZero_returns_9876()
+test_int_rc_text_positiveOrZero_returns_neg333()
+test_int_rc_text_is_not_positiveOrZero_returns_333()
+test_int_rc_text_is_not_positiveOrZero_returns_321()
+test_int_rc_text_is_not_positiveOrZero_returns_3456()
