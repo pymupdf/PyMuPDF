aea7bb3128d968662b2deaf6db96459ee153aab2
erinelgz <e.elagoz@student.vu.nl>
2024-06-14 14:12:23 +0200

    2 Files improved - erin

--- tests/test_get_env_int.py
+++ tests/test_get_env_int.py
@@ -6,9 +6,6 @@ branch_coverage = {
     "branch_2": False
 }
 
-def log(message):
-    print(message)
-
 def get_env_int( name, default):
     v = os.environ.get( name)
     if v is None:
@@ -17,18 +14,28 @@ def get_env_int( name, default):
     else:
         branch_coverage["branch_2"] = True
         ret = int(v)
-    if ret != default:
-        log(f'Using non-default setting from {name}: {v}')
     return ret
 
 def print_coverage():
     for branch, hit in branch_coverage.items():
         print(f"{branch} was {'hit' if hit else 'not hit'}")
 
-os.environ['VAR'] = '1'
-result = get_env_int('VAR', 1)
-print_coverage()
 
-del os.environ['VAR']
-result = get_env_int('VAR', 0)
-print_coverage()
+def test_get_env_int_default_is_not_None_returns_1():
+    os.environ['VAR'] = '1'
+    result = get_env_int('VAR', 1)
+    print_coverage()
+
+def test_get_env_int_default_is_not_None_returns_0():
+    os.environ['VAR'] = '0'
+    result = get_env_int('VAR', 1)
+    print_coverage()
+
+def test_get_env_int_default_isNone_returns_default():
+    del os.environ['VAR']
+    result = get_env_int('VAR', 0)
+    print_coverage()
+
+test_get_env_int_default_is_not_None_returns_1()
+test_get_env_int_default_is_not_None_returns_0()
+test_get_env_int_default_isNone_returns_default()
