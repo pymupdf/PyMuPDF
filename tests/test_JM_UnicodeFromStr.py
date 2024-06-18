e111957a8ee2e4ad642bc8e3810845f7e8a97165
misraozoktas <misra.ozoktas@gmail.com>
2024-06-18 20:25:28 +0200

    Two fixed test files added

--- tests/test_JM_UnicodeFromStr.py
+++ tests/test_JM_UnicodeFromStr.py
@@ -20,23 +20,8 @@ def print_coverage():
     for branch, hit in branch_coverage.items():
         print(f"{branch} was {'hit' if hit else 'not hit'}")
 
-def test_JM_UnicodeFromStr_s_is_none_returns_none():
-    result = JM_UnicodeFromStr(None)
-    print_coverage()
+result = JM_UnicodeFromStr(None)
+print_coverage()
 
-def test_JM_UnicodeFromStr_s_is_byte_xxx_returns_decoded_s():
-    result = JM_UnicodeFromStr(b"xxx")
-    print_coverage()
-
-def test_JM_UnicodeFromStr_s_is_byte_abcdefg_returns_decoded_s():
-    result = JM_UnicodeFromStr(b"abcdefg")
-    print_coverage()
-
-def test_JM_UnicodeFromStr_s_is_string_xxx_returns_same_s():
-    result = JM_UnicodeFromStr("xxx")
-    print_coverage()
-
-test_JM_UnicodeFromStr_s_is_none_returns_none()
-test_JM_UnicodeFromStr_s_is_byte_xxx_returns_decoded_s()
-test_JM_UnicodeFromStr_s_is_byte_abcdefg_returns_decoded_s()
-test_JM_UnicodeFromStr_s_is_string_xxx_returns_same_s()
+result = JM_UnicodeFromStr(b"xxx")
+print_coverage()
