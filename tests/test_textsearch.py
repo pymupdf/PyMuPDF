2414c806ca47cc01c7d2552f09dcbf2c7dc891f8
Jorj X. McKie <jorj.x.mckie@outlook.de>
2024-07-13 07:38:40 -0400

    No ligature preservation in searches
    Specifying ligatures in search needles makes no sense. Not setting the flag bit causes text to be found with and without ligatures.

--- tests/test_textsearch.py
+++ tests/test_textsearch.py
@@ -7,6 +7,7 @@ rectangle with the searched text.
 Text search with 'clip' parameter - clip rectangle contains two occurrences
 of searched text. Confirm search locations are inside clip.
 """
+
 import os
 
 import pymupdf
@@ -14,6 +15,7 @@ import pymupdf
 scriptdir = os.path.abspath(os.path.dirname(__file__))
 filename1 = os.path.join(scriptdir, "resources", "2.pdf")
 filename2 = os.path.join(scriptdir, "resources", "github_sample.pdf")
+filename3 = os.path.join(scriptdir, "resources", "text-find-ligatures.pdf")
 
 
 def test_search1():
@@ -35,3 +37,16 @@ def test_search2():
     assert len(rl) == 2
     for r in rl:
         assert r in clip
+
+
+def test_search3():
+    """Ensure we find text whether or not it contains ligatures."""
+    doc = pymupdf.open(filename3)
+    page = doc[0]
+    needle = "flag"
+    hits = page.search_for(needle, flags=pymupdf.TEXTFLAGS_SEARCH)
+    assert len(hits) == 2  # all occurrences found
+    hits = page.search_for(
+        needle, flags=pymupdf.TEXTFLAGS_SEARCH | pymupdf.TEXT_PRESERVE_LIGATURES
+    )
+    assert len(hits) == 1  # only found text without ligatures
