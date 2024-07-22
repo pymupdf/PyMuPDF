58174fb203e7875ba4f128122e5510dbed44fb73
Julian Smith <julian.smith@artifex.com>
2024-07-22 09:01:17 +0100

    src/__init__.py: log(): make slightly more robust.

--- src/__init__.py
+++ src/__init__.py
@@ -80,15 +80,22 @@ def log( text='', caller=1):
     '''
     For development/debugging diagnostics.
     '''
-    frame_record = inspect.stack( context=0)[ caller]
-    filename    = os.path.relpath(frame_record.filename)
-    line        = frame_record.lineno
-    function    = frame_record.function
-    text = f'{filename}:{line}:{function}: {text}'
+    try:
+        stack = inspect.stack(context=0)
+    except StopIteration:
+        pass
+    else:
+        frame_record = stack[caller]
+        try:
+            filename = os.path.relpath(frame_record.filename)
+        except Exception:   # Can fail on windows.
+            filename = frame_record.filename
+        line = frame_record.lineno
+        function = frame_record.function
+        text = f'{filename}:{line}:{function}(): {text}'
     if _g_log_items_active:
         _g_log_items.append(text)
-    print(text, file=_g_out_log)
-    _g_out_log.flush()
+    print(text, file=_g_out_log, flush=1)
 
 
 def message(text=''):
