edb4632b6c1a85abeee5f9644b45ec71ec91b105
Julian Smith <julian.smith@artifex.com>
2024-10-10 00:14:34 +0100

    src/__init__.py: jm_lineart_path(): add comment explaining use of mupdf.fz_walk_path().

--- src/__init__.py
+++ src/__init__.py
@@ -18922,6 +18922,11 @@ def jm_lineart_path(dev, ctx, path):
         # globally, each time takes 0.3s.
         #
         walker = Walker(dev)
+        # Unlike fz_run_page(), fz_path_walker callbacks are not passed
+        # a pointer to the struct, instead they get an arbitrary
+        # void*. The underlying C++ Director callbacks use this void* to
+        # identify the fz_path_walker instance so in turn we need to pass
+        # arg=walker.m_internal.
         mupdf.fz_walk_path( mupdf.FzPath(mupdf.ll_fz_keep_path(path)), walker, walker.m_internal)
         # Check if any items were added ...
         if not dev.pathdict[ dictkey_items]:
