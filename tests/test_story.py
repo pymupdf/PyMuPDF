49b028aef2b23e4e68fa19a0cc7f6c0ced44e954
Julian Smith <julian.smith@artifex.com>
2024-10-31 23:26:17 +0000

    tests/test_story.py: added test for #3813.
    This is addressed in mupdf master.

--- tests/test_story.py
+++ tests/test_story.py
@@ -227,3 +227,68 @@ def test_write_stabilized_with_links():
 def test_archive_creation():
     s = pymupdf.Story(archive=pymupdf.Archive('.'))
     s = pymupdf.Story(archive='.')
+
+
+def test_3813():
+    import pymupdf
+
+    HTML = """
+    <p>Count is fine:</p>
+    <ol>
+        <li>Lorem
+            <ol>
+                <li>Sub Lorem</li>
+                <li>Sub Lorem</li>
+            </ol>
+        </li>
+        <li>Lorem</li>
+        <li>Lorem</li>
+    </ol>
+
+    <p>Broken count:</p>
+    <ol>
+        <li>Lorem
+            <ul>
+                <li>Sub Lorem</li>
+                <li>Sub Lorem</li>
+            </ul>
+        </li>
+        <li>Lorem</li>
+        <li>Lorem</li>
+    </ol>
+    """
+    MEDIABOX = pymupdf.paper_rect("A4")
+    WHERE = MEDIABOX + (36, 36, -36, -36)
+
+    story = pymupdf.Story(html=HTML)
+    path = os.path.normpath(f'{__file__}/../../tests/test_3813_out.pdf')
+    writer = pymupdf.DocumentWriter(path)
+
+    more = 1
+
+    while more:
+        device = writer.begin_page(MEDIABOX)
+        more, _ = story.place(WHERE)
+        story.draw(device)
+        writer.end_page()
+
+    writer.close()
+    
+    with pymupdf.open(path) as document:
+        page = document[0]
+        text = page.get_text()
+    text_utf8 = text.encode()
+    
+    if pymupdf.mupdf_version_tuple < (1, 25):
+        # MuPDF gets things wrong.
+        text_expected_utf8 = b'Count is \xef\xac\x81ne:\n1. Lorem\n1. Sub Lorem\n2. Sub Lorem\n2. Lorem\n3. Lorem\nBroken count:\n1. Lorem\n\xe2\x80\xa2  Sub Lorem\n\xe2\x80\xa2  Sub Lorem\n4. Lorem\n5. Lorem\n'
+    else:
+        text_expected_utf8 = b'Count is \xef\xac\x81ne:\n1. Lorem\n1. Sub Lorem\n2. Sub Lorem\n2. Lorem\n3. Lorem\nBroken count:\n1. Lorem\n\xe2\x80\xa2  Sub Lorem\n\xe2\x80\xa2  Sub Lorem\n2. Lorem\n3. Lorem\n'
+    text_expected = text_expected_utf8.decode()
+    
+    print(f'text_utf8:\n    {text_utf8!r}')
+    print(f'text_expected_utf8:\n    {text_expected_utf8!r}')
+    print(f'text:\n    {textwrap.indent(text, "    ")}')
+    print(f'text_expected:\n    {textwrap.indent(text_expected, "   ")}')
+    
+    assert text == text_expected
