928c5dc2aa3aebcce6d405802b7589aaf14faa99
Julian Smith <julian.smith@artifex.com>
2025-02-04 16:33:35 +0000

    tests/: fixes for test_1645() and recent mupdf.
    Removed expected outputs for older mupdf versions. mupdf master and 1.25.x both
    behave identically now, and PyMuPDF now shows the text annotation.

--- tests/test_annots.py
+++ tests/test_annots.py
@@ -230,15 +230,7 @@ def test_1645():
     pymupdf.TOOLS.set_annot_stem('jorj')
     try:
         path_in = os.path.abspath( f'{__file__}/../resources/symbol-list.pdf')
-
-        if pymupdf.mupdf_version_tuple >= (1, 25, 4):
-            path_expected = os.path.abspath( f'{__file__}/../../tests/resources/test_1645_expected_1.25.4.pdf')
-        elif pymupdf.mupdf_version_tuple >= (1, 25):
-            path_expected = os.path.abspath( f'{__file__}/../../tests/resources/test_1645_expected_1.25.pdf')
-        elif pymupdf.mupdf_version_tuple >= (1, 24, 2):
-            path_expected = os.path.abspath( f'{__file__}/../../tests/resources/test_1645_expected_1.24.2.pdf')
-        else:
-            path_expected = os.path.abspath( f'{__file__}/../../tests/resources/test_1645_expected_1.24.pdf')
+        path_expected = os.path.abspath( f'{__file__}/../../tests/resources/test_1645_expected.pdf')
         path_out = os.path.abspath( f'{__file__}/../test_1645_out.pdf')
         doc = pymupdf.open(path_in)
         page = doc[0]
