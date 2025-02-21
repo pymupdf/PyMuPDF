261415e2a0550037f41e8f6918f44a6a3d205a56
Jorj X. McKie <jorj.x.mckie@outlook.de>
2025-02-21 09:40:01 -0400

    Handle duplicate widget names when joining PDFs
    Previously, in some constellations duplicated fields on source pages (i.e. having a common parent) were not correctly copied over to the target PDF.
    In addition, when source widgets happened to have same names as present in the target, invalid widgets resulted in the merged PDF.
    
    This fix copies all root widgets to the target in a separate first step.  Only any root fields in source pages are separately copied over.
    
    Finally, name duplicates are resolved by either renaming source widgets, or (option) joining them with target widgets in common `/Kids` arrays.

--- tests/test_insertpdf.py
+++ tests/test_insertpdf.py
@@ -10,6 +10,7 @@ import io
 import os
 import re
 import pymupdf
+from pymupdf import mupdf
 
 scriptdir = os.path.abspath(os.path.dirname(__file__))
 resources = os.path.join(scriptdir, "resources")
@@ -54,7 +55,7 @@ def approx_compare( a, b, max_delta):
     if ret:
         print( f'Differ:\n    a={a!r}\n    b={b!r}')
     return ret
-        
+
 
 def test_insert():
     all_text_original = []  # text on input pages
@@ -132,7 +133,7 @@ def _2861_2871_merge_pdf(content: bytes, coverpage: bytes):
         with pymupdf.Document(stream=content, filetype="pdf") as content_pdf:
             coverpage_pdf.insert_pdf(content_pdf)
             doc = coverpage_pdf.write()
-            return doc 
+            return doc
 
 def test_2861():
     path = os.path.abspath(f'{__file__}/../../tests/resources/test_2861.pdf')
@@ -152,16 +153,16 @@ def test_2871():
 
 
 def test_3789():
-    
+
     file_path = os.path.abspath(f'{__file__}/../../tests/resources/test_3789.pdf')
     result_path = os.path.abspath(f'{__file__}/../../tests/test_3789_out')
     pages_per_split = 5
-    
+
     # Clean pdf
     doc = pymupdf.open(file_path)
     tmp = io.BytesIO()
     tmp.write(doc.write(garbage=4, deflate=True))
-    
+
     source_doc = pymupdf.Document('pdf', tmp.getvalue())
     tmp.close()
 
@@ -190,8 +191,7 @@ def test_3789():
 
 def test_widget_insert():
     """Confirm copy of form fields / widgets."""
-    from pymupdf import mupdf
-    tar = pymupdf.open(os.path.join(resources, "cms-etc-filled.pdf"))
+    tar = pymupdf.open(os.path.join(resources, "merge-form1.pdf"))
     pc0 = tar.page_count  # for later assertion
     src = pymupdf.open(os.path.join(resources, "interfield-calculation.pdf"))
     pc1 = src.page_count  # for later assertion
@@ -221,3 +221,80 @@ def test_widget_insert():
     assert tar.page_count == pc0 + pc1
     assert new_field_count == tar_field_count + src_field_count
     assert new_co_count == tar_co_count + src_co_count
+
+
+def names_and_kids(doc):
+    """Return a list of dictionaries with keys "name" and "kids".
+
+    "name" is the name of a root field in "Root/AcroForm/Fields", and
+    "kids" is the count of its immediate children.
+    """
+    rc = []
+    pdf = pymupdf._as_pdf_document(doc)
+    fields = mupdf.pdf_dict_getl(
+        mupdf.pdf_trailer(pdf),
+        pymupdf.PDF_NAME("Root"),
+        pymupdf.PDF_NAME("AcroForm"),
+        pymupdf.PDF_NAME("Fields"),
+    )
+    if not fields.pdf_is_array():
+        return rc
+    root_count = fields.pdf_array_len()
+    if not root_count:
+        return rc
+    for i in range(root_count):
+        field = fields.pdf_array_get(i)
+        kids = field.pdf_dict_get(pymupdf.PDF_NAME("Kids"))
+        kid_count = kids.pdf_array_len()
+        T = field.pdf_dict_get_text_string(pymupdf.PDF_NAME("T"))
+        field_dict = {"name": T, "kids": kid_count}
+        rc.append(field_dict)
+    return rc
+
+
+def test_merge_checks1():
+    """Merge Form PDFs making any duplicate names unique."""
+    merge_file1 = os.path.join(resources, "merge-form1.pdf")
+    merge_file2 = os.path.join(resources, "merge-form2.pdf")
+    tar = pymupdf.open(merge_file1)
+    rc0 = names_and_kids(tar)
+    src = pymupdf.open(merge_file2)
+    rc1 = names_and_kids(src)
+    tar.insert_pdf(src, join_duplicates=False)
+    rc2 = names_and_kids(tar)
+    assert len(rc2) == len(rc0) + len(rc1)
+
+
+def test_merge_checks2():
+    # Join / merge Form PDFs joining any duplicate names in the src PDF.
+    merge_file1 = os.path.join(resources, "merge-form1.pdf")
+    merge_file2 = os.path.join(resources, "merge-form2.pdf")
+    tar = pymupdf.open(merge_file1)
+    rc0 = names_and_kids(tar)  # list of root names and kid counts
+    names0 = [itm["name"] for itm in rc0]  # root names in target
+    kids0 = sum([itm["kids"] for itm in rc0])  # number of kids in target
+
+    src = pymupdf.open(merge_file2)
+    rc1 = names_and_kids(src)  # list of root namesand kids in source PDF
+    dup_count = 0  # counts duplicate names in source PDF
+    dup_kids = 0  # counts the expected kids after merge
+
+    for itm in rc1:  # walk root fields of source pdf
+        if itm["name"] not in names0:  # not a duplicate name
+            continue
+        # if target field has kids, add their count, else add 1
+        dup_kids0 = sum([i["kids"] for i in rc0 if i["name"] == itm["name"]])
+        dup_kids += dup_kids0 if dup_kids0 else 1
+        # if source field has kids add their count, else add 1
+        dup_kids += itm["kids"] if itm["kids"] else 1
+
+    names1 = [itm["name"] for itm in rc1]  # names in source
+
+    tar.insert_pdf(src, join_duplicates=True)  # join merging any duplicate names
+
+    rc2 = names_and_kids(tar)  # get names and kid counts in resulting PDF
+    names2 = [itm["name"] for itm in rc2]  # resulting names in target
+    kids2 = sum([itm["kids"] for itm in rc2])  # total resulting kid count
+
+    assert len(set(names0 + names1)) == len(names2)
+    assert kids2 == dup_kids
