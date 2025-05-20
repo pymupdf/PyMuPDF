2115c874934738289c96649877dca7136270b566
Julian Smith <julian.smith@artifex.com>
2025-05-20 12:10:19 +0100

    Update version numbers.

--- setup.py
+++ setup.py
@@ -1229,15 +1229,15 @@ classifier = [
 #
 
 # PyMuPDF version.
-version_p = '1.25.5'
+version_p = '1.26.0'
 
 # PyMuPDFb version. This is the PyMuPDF version whose PyMuPDFb wheels we will
 # (re)use if generating separate PyMuPDFb wheels. Though as of PyMuPDF-1.24.11
 # (2024-10-03) we no longer use PyMuPDFb wheels so this is actually unused.
 #
-version_b = '1.25.3'
+version_b = '1.26.0'
 
-version_mupdf = '1.25.6'
+version_mupdf = '1.26.1'
 
 if os.path.exists(f'{g_root}/{g_pymupdfb_sdist_marker}'):
     
