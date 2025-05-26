75e2c66b4c412d078c604bbf1d1c585d4f7bf095
Julian Smith <julian.smith@artifex.com>
2025-05-26 16:26:07 +0100

    tests/test_4520.py: simplified test to only expect current behaviour.

--- tests/test_4520.py
+++ tests/test_4520.py
@@ -3,17 +3,9 @@ import pymupdf
 
 def test_4520():
     """Accept source pages without /Contents object in show_pdf_page."""
-    vsn_tuple = tuple(map(int, pymupdf.__version__.split(".")))
     tar = pymupdf.open()
     src = pymupdf.open()
     src.new_page()
     page = tar.new_page()
-    try:
-        assert page.show_pdf_page(page.rect, src, 0)
-        rc = True
-    except Exception as e:
-        rc = False
-    if vsn_tuple < (1, 26, 0):
-        assert rc is False
-    else:
-        assert rc is True
+    xref = page.show_pdf_page(page.rect, src, 0)
+    assert xref
