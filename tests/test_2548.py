f67a31887a0d35895776d1a6dd86b5f5477b4c88
Sebastian Rasmussen <sebras@gmail.com>
2025-07-29 13:13:41 +0200

    tests/test_{2548,textextract}.py: fix expected output from MuPDF 1.27.x.

--- tests/test_2548.py
+++ tests/test_2548.py
@@ -32,7 +32,10 @@ def test_2548():
     # This checks that PyMuPDF 1.23.7 fixes this bug, and also that earlier
     # versions with updated MuPDF also fix the bug.
     rebased = hasattr(pymupdf, 'mupdf')
-    expected = 'format error: cycle in structure tree\nstructure tree broken, assume tree is missing'
+    if pymupdf.mupdf_version_tuple >= (1, 27):
+        expected = 'format error: No common ancestor in structure tree\nstructure tree broken, assume tree is missing\nformat error: No common ancestor in structure tree\nstructure tree broken, assume tree is missing\nformat error: No common ancestor in structure tree\nstructure tree broken, assume tree is missing\nformat error: No common ancestor in structure tree\nstructure tree broken, assume tree is missing\nformat error: No common ancestor in structure tree\nstructure tree broken, assume tree is missing'
+    else:
+        expected = 'format error: cycle in structure tree\nstructure tree broken, assume tree is missing'
     if rebased:
         assert wt == expected, f'expected:\n    {expected!r}\nwt:\n    {wt!r}\n'
     assert not e
