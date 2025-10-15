4578a8f0380e7cd55bc1f7531e71c13a9069919b
Jorj X. McKie <jorj.x.mckie@outlook.de>
2025-10-15 10:15:44 -0400

    more changes
    Support pymupdf_layout

--- tests/test_linequad.py
+++ tests/test_linequad.py
@@ -2,6 +2,7 @@
 Check approx. equality of search quads versus quads recovered from
 text extractions.
 """
+
 import os
 
 import pymupdf
@@ -11,12 +12,14 @@ filename = os.path.join(scriptdir, "resources", "quad-calc-0.pdf")
 
 
 def test_quadcalc():
+    old_settings = pymupdf.TOOLS.unset_quad_corrections()
+    pymupdf.TOOLS.unset_quad_corrections(False)
     text = " angle 327"  # search for this text
     doc = pymupdf.open(filename)
     page = doc[0]
     # This special page has one block with one line, and
     # its last span contains the searched text.
-    block = page.get_text("dict", flags=0)["blocks"][0]
+    block = page.get_text("dict")["blocks"][0]
     line = block["lines"][0]
     # compute quad of last span in line
     lineq = pymupdf.recover_line_quad(line, spans=line["spans"][-1:])
@@ -28,3 +31,4 @@ def test_quadcalc():
     assert abs(searchq.ur - lineq.ur) <= 1e-4
     assert abs(searchq.ll - lineq.ll) <= 1e-4
     assert abs(searchq.lr - lineq.lr) <= 1e-4
+    pymupdf.TOOLS.unset_quad_corrections(old_settings)
