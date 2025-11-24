fc521525ecf1192335cd55c380f943fc6e12c3c9
Julian Smith <julian.smith@artifex.com>
2025-11-24 11:32:12 +0000

    ghtest.py temporary commit

--- scripts/sysinstall.py
+++ scripts/sysinstall.py
@@ -401,6 +401,7 @@ def main():
                 'test_textbox3',
                 'test_3493',
                 'test_4180',
+                'test_4767',
                 )
         excluded_tests = ' and not '.join(excluded_tests)
         if not pytest_args:
