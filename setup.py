e68755ff3fcf93b240a6ba41046fe34be33905d8
Julian Smith <julian.smith@artifex.com>
2025-12-02 15:29:10 +0000

    setup.py changes.txt: upgrade to mupdf-1.26.12 by default.

--- setup.py
+++ setup.py
@@ -1278,7 +1278,7 @@ classifier = [
 # PyMuPDF version.
 version_p = '1.26.7'
 
-version_mupdf = '1.26.11'
+version_mupdf = '1.26.12'
 
 # PyMuPDFb version. This is the PyMuPDF version whose PyMuPDFb wheels we will
 # (re)use if generating separate PyMuPDFb wheels. Though as of PyMuPDF-1.24.11
