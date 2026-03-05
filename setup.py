d6fb27d64e42f2058cfbc66177cc9feb77dc44a9
Julian Smith <julian.smith@artifex.com>
2026-03-05 15:51:32 +0000

    Updates for release of 1.27.2.

--- setup.py
+++ setup.py
@@ -1298,9 +1298,9 @@ classifier = [
 #
 
 # PyMuPDF version.
-version_p = '1.27.1'
+version_p = '1.27.2'
 
-version_mupdf = '1.27.1'
+version_mupdf = '1.27.2'
 
 # PyMuPDFb version. This is the PyMuPDF version whose PyMuPDFb wheels we will
 # (re)use if generating separate PyMuPDFb wheels. Though as of PyMuPDF-1.24.11
