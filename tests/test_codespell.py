01772447de935e250bb97f918dc880ba42b0f4ee
Julian Smith <julian.smith@artifex.com>
2026-03-23 14:34:45 +0000

    tests/test_codespell.py: reduce length of codespell command line.
    We use codespell's new @FILE arg.

--- tests/test_codespell.py
+++ tests/test_codespell.py
@@ -49,20 +49,28 @@ def test_codespell():
         import pipcl
     finally:
         del sys.path[0]
+    
     git_files = pipcl.git_items(root)
     
-    for p in git_files:
-        _, ext = os.path.splitext(p)
-        if ext in ('.png', '.pdf', '.jpg', '.svg'):
-            pass
-        else:
-            command += f'    {p}\n'
+    command_args_path = os.path.normpath(f'{__file__}/../test_codespell_args.txt')
+    command += f'    @{command_args_path}'
+    
+    with open(command_args_path, 'w') as f:
+    
+        for p in git_files:
+            _, ext = os.path.splitext(p)
+            if ext in ('.png', '.pdf', '.jpg', '.svg'):
+                pass
+            else:
+                #command += f'    {p}\n'
+                print(p, file=f)
     
     if platform.system() != 'Windows':
         command = command.replace('\n', ' \\\n')
-    # Don't print entire command because very long, and will be displayed
-    # anyway if there is an error.
-    #print(f'test_codespell(): Running: {command}')
-    print(f'Running codespell.')
+    if 0:
+        with open(command_args_path) as f:
+            command_args_path_contents = f.read()
+        print(f'command_args_path:{command_args_path_contents}')
+    print(f'Running codespell: {command}')
     subprocess.run(command, shell=1, check=1)
     print('test_codespell(): codespell succeeded.')
