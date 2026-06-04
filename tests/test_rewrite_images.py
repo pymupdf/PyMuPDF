783b327af59d51747b805e62b01013df7079d843
Julian Smith <julian.smith@artifex.com>
2026-06-04 01:04:29 +0100

    tests/test_rewrite_images.py: added test_4918().

--- tests/test_rewrite_images.py
+++ tests/test_rewrite_images.py
@@ -1,5 +1,7 @@
 import pymupdf
 import os
+import util
+
 
 scriptdir = os.path.dirname(__file__)
 
@@ -13,3 +15,25 @@ def test_rewrite_images():
     data = doc.tobytes(garbage=3, deflate=True)
     size1 = len(data)
     assert (1 - (size1 / size0)) > 0.3
+
+
+def test_4918():
+    '''
+    By default this test does nothing, because it requires a rather large input document from:
+        https://drive.google.com/file/d/1OkIq3XJuKiFfKDWBIcAk8_fLLjpNkuHQ/view?usp=sharing
+    
+    It's non-trivial to download from this url, so we only do anything if
+    environment variable PYMUPDF_TEST_4918_PATH is set to local path of the
+    input document.
+    
+    As of 2026-06-04 this passes with mupdf master, but segvs with current
+    pymupdf release 1.27.2.3.
+    '''
+    PYMUPDF_TEST_4918_PATH = os.environ.get('PYMUPDF_TEST_4918_PATH')
+    if not PYMUPDF_TEST_4918_PATH :
+        print(f'test_4918(): Doing nothing because {PYMUPDF_TEST_4918_PATH=}.')
+        return
+    path = PYMUPDF_TEST_4918_PATH
+    print(f'{path=}')
+    with pymupdf.open(path) as document:
+        document.rewrite_images(dpi_threshold=150, dpi_target=100, quality=50)
